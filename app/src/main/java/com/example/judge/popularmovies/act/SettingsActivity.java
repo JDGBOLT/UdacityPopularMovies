b54f28534abe8a9faf1c358c18be2cfa450847a2
Joshua Gwinn <jdgbolt@gmail.com>
2015-08-23 20:52:09 -0700

    Change Settings activity to have a toolbar with a back button

--- app/src/main/java/com/example/judge/popularmovies/act/SettingsActivity.java
+++ app/src/main/java/com/example/judge/popularmovies/act/SettingsActivity.java
@@ -4,10 +4,13 @@
 
 package com.example.judge.popularmovies.act;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
 
-import com.example.judge.popularmovies.frag.SettingsFragment;
+import com.example.judge.popularmovies.R;
 
 
 /**
@@ -19,8 +22,18 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
-                new SettingsFragment()).commit();
+        setContentView(R.layout.activity_settings);
+
+        // These are required in order to have a toolbar in the settings pane
+        Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Since we are using a transparent toolbar, set the color to the correct color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.primary_dark));
+        }
     }
 
 }
